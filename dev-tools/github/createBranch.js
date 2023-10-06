9d0245e87546007af9541ea0071cb59aa20c7ace
Kirk Robertson <kirk.robertson@iag.com.au>
2023-10-06 14:24:58 +1100

    test

--- dev-tools/github/createBranch.js
+++ dev-tools/github/createBranch.js
@@ -1,33 +1,48 @@
-import { Octokit } from "@octokit/core";
+// import { Octokit } from "@octokit/core";
+// //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
+// const runCreateBranch = async () => {
+//     try {
+//         // const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+//         const octokit = new Octokit({ auth: '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' })
+//         const { data } = await octokit.request("/user");
+//         console.log(">>sdfsddf", data)
+//         // const owner = octokit.getInput('owner', { required: true });
+//         // const repo = octokit.getInput('repo', { required: true });
+        
+//         // const responseListPullRequest = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+//         //     owner,
+//         //     repo,
+//         //     headers: {
+//         //       'X-GitHub-Api-Version': '2022-11-28'
+//         //     }
+//         //   })
+          
+//         //   const pullNumber = responseListPullRequest.data[0].number
+//         //   const commentOnPR = await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+//         //     owner,
+//         //     repo,
+//         //     issue_number: pullNumber,
+//         //     body: `FROM ACTIONS ![im an alt](i am an image) <b>hellloo</b>`,
+//         //     headers: {
+//         //       'X-GitHub-Api-Version': '2022-11-28'
+//         //     }
+//         //   })
 
+
+//     } catch (error) {
+//         console.log(">>ERROR", error)
+//     }
+// }
+
+import { execSync } from "child_process"
+
+import path from "path"
 const runCreateBranch = async () => {
     try {
-        const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
-        const owner = octokit.getInput('owner', { required: true });
-        const repo = octokit.getInput('repo', { required: true });
-        
-        const responseListPullRequest = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
-            owner,
-            repo,
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28'
-            }
-          })
-          
-          const pullNumber = responseListPullRequest.data[0].number
-          const commentOnPR = await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-            owner,
-            repo,
-            issue_number: pullNumber,
-            body: `FROM ACTIONS ![im an alt](i am an image) <b>hellloo</b>`,
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28'
-            }
-          })
-
-
+        // run loki
+        execSync(path.join(__dirname, '../../node_modules/.bin/loki'))
     } catch (error) {
-        console.log(">>ERROR", error)
+        console.log(">>error", error)
     }
 }
 
